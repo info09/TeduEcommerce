239cbb5be5d4bfe08055e952f5b1fb05cfc90429
Quang Huy Trần <huytq3103@gmail.com>
2023-03-29 23:30:02 +0700

    finish folder structure

---
+++ aspnet-core/src/TeduEcommerce.Admin.Application.Contracts/Permissions/TeduEcommerceAdminPermissions.cs
@@ -0,0 +1,9 @@
+﻿namespace TeduEcommerce.Admin.Permissions;
+
+public static class TeduEcommerceAdminPermissions
+{
+    public const string GroupName = "TeduEcommerce";
+
+    //Add your own permission names. Example:
+    //public const string MyPermission1 = GroupName + ".MyPermission1";
+}
