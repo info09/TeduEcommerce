be366bc0e0c1e132d1b253f06aafd3de4742ddb2
Quang Huy Trần <huytq3103@gmail.com>
2023-06-24 15:37:43 +0700

    gắn module đăng nhập

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
     <router-outlet></router-outlet>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  menuMode = 'static';
+
+  constructor(private primengConfig: PrimeNGConfig){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.primengConfig.ripple = true;
+    document.documentElement.style.fontSize = '14px';
+  }
+}
