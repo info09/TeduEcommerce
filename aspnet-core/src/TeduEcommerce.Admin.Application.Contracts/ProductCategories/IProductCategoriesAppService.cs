b3fbd8c14a0caac7f8c181f8603dcc0a96eee20e
Quang Huy Trần <huytq3103@gmail.com>
2023-07-12 23:17:44 +0700

    Backend CRUD ProductCategory

---
+++ aspnet-core/src/TeduEcommerce.Admin.Application.Contracts/ProductCategories/IProductCategoriesAppService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace TeduEcommerce.Admin.ProductCategories
+{
+    public interface IProductCategoriesAppService : ICrudAppService<ProductCategoryDto, Guid, PagedResultRequestDto, CreateUpdateProductCategoryDto, CreateUpdateProductCategoryDto>
+    {
+    }
+}
---
+++ aspnet-core/src/TeduEcommerce.Admin.Application/ProductCategories/ProductCategoriesAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TeduEcommerce.ProductCategories;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace TeduEcommerce.Admin.ProductCategories
+{
+    public class ProductCategoriesAppService : CrudAppService<ProductCategory, ProductCategoryDto, Guid, PagedResultRequestDto, CreateUpdateProductCategoryDto, CreateUpdateProductCategoryDto>, IProductCategoriesAppService
+    {
+        public ProductCategoriesAppService(IRepository<ProductCategory, Guid> repository) : base(repository)
+        {
+        }
+    }
+}
