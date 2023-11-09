bcf6bf83509d37d2c1975fd8aac27e629dc46f0b
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2023-11-09 16:26:11 +0700

    Add files via upload
    6.4

---
+++ aspnet-core/src/TeduEcommerce.Public.Application.Contracts/Catalog/Manufacturers/IManufactuereAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace TeduEcommerce.Public.Catalog.Manufacturers
+{
+    public interface IManufactuereAppService : IReadOnlyAppService<ManufacturerDto, Guid, PagedResultRequestDto>
+    {
+        Task<PagedResultDto<ManufacturerInListDto>> GetListFilterAsync(BaseListFilterDto input);
+        Task<List<ManufacturerInListDto>> GetListAllAsync();
+    }
+}
---
+++ aspnet-core/src/TeduEcommerce.Public.Application/Catalog/ProductAttributes/ProductAttributeAppService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeduEcommerce.ProductAttributes;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace TeduEcommerce.Public.Catalog.ProductAttributes
+{
+    public class ProductAttributeAppService : ReadOnlyAppService<ProductAttribute, ProductAttributeDto, Guid, PagedResultRequestDto>, IProductAttributeAppService
+    {
+        public ProductAttributeAppService(IReadOnlyRepository<ProductAttribute, Guid> repository) : base(repository)
+        {
+        }
+
+        public async Task<List<ProductAttributeInListDto>> GetListAllAsync()
+        {
+            var query = await Repository.GetQueryableAsync();
+            query = query.Where(i => i.IsActive);
+
+            var data = await AsyncExecuter.ToListAsync(query);
+            return ObjectMapper.Map<List<ProductAttribute>, List<ProductAttributeInListDto>>(data);
+        }
+
+        public async Task<PagedResultDto<ProductAttributeInListDto>> GetListFilterAsync(BaseListFilterDto input)
+        {
+            var query = await Repository.GetQueryableAsync();
+            query = query.WhereIf(!string.IsNullOrWhiteSpace(input.Keyword), i => i.Label.ToLower().Contains(input.Keyword.ToLower()));
+
+            var totalCount = await AsyncExecuter.LongCountAsync(query);
+
+            var data = await AsyncExecuter.ToListAsync(query.OrderByDescending(i => i.CreationTime).Skip(input.SkipCount).Take(input.MaxResultCount));
+
+            return new PagedResultDto<ProductAttributeInListDto>(totalCount, ObjectMapper.Map<List<ProductAttribute>, List<ProductAttributeInListDto>>(data));
+        }
+    }
+}
