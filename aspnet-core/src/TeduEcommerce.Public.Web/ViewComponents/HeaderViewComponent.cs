b9209a35963d3f47cfc3d19a3110082c37e6a874
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2023-11-09 16:28:13 +0700

    Add files via upload

---
+++ aspnet-core/src/TeduEcommerce.Public.Web/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TeduEcommerce.Public.Catalog.ProductCategories;
+
+namespace TeduEcommerce.Public.Web.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly IProductCategoryAppService _productCategoryAppService;
+
+        public HeaderViewComponent(IProductCategoryAppService productCategoryAppService)
+        {
+            this._productCategoryAppService = productCategoryAppService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _productCategoryAppService.GetListAllAsync();
+            return View(model);
+        }
+    }
+}
